119e7fee05e75cb277df47d3b81bc762818bfd06
SyntaxErratum <jared@edatsol.com>
2019-07-29 12:42:09 -0600

    Added mock data

---
+++ src/app/currency-data.service.ts
@@ -0,0 +1,13 @@
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { Currency } from './models/currency.model';
+
+// @Injectable({
+//     providedIn: 'root'
+// })
+
+// export Class CurrencyDataService {
+//     getMockData(): Observable<Currency[]> {
+//
+//     }
+// }
--- src/app/top-currencies/top-currencies.component.ts
+++ src/app/top-currencies/top-currencies.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Currency } from '../models/currency.model';
 
 @Component({
     selector: 'app-top-currencies',
@@ -6,4 +7,38 @@ import { Component } from '@angular/core';
     styleUrls: ['./top-currencies.component.scss']
 })
 
-export class TopCurrenciesComponent {}
+export class TopCurrenciesComponent {
+    topList: Currency[];
+
+    ngOnInit(): void {
+        this.topList = this.getCurrencies();
+    }
+
+    getCurrencies() {
+        const mockData =
+        [
+            new Currency(
+                'BTC',
+                'Bitcoin',
+                '10000000',
+                '999999999999',
+                Date.now
+            ),
+            new Currency(
+                'ETH',
+                'Ethereum',
+                '10000',
+                '99999999999',
+                Date.now
+            ),
+            new Currency(
+                'LTC',
+                'Litecoin',
+                '2000',
+                '9999999999',
+                Date.now
+            )
+        ];
+        return mockData;
+    }
+}
